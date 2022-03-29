a03baf5cd7d8160a8d631067731374078517dabf
jayde225 <jayde225@users.noreply.github.com>
2022-03-29 09:52:13 -0500

    Created Movie class and created all needed variables

---
+++ src/Movie.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Movie {
+    String title;
+    Integer year;
+    String maturityRating;
+    String releaseDate;
+    String runTime;
+    String genre;
+    ArrayList<String> movieGenres = new ArrayList<String>(); //Used to store all genres for a movie
+    String director;
+    String writer;
+    ArrayList<String> movieWriters = new ArrayList<String>(); //Used to store all writes for a movie
+    String actor;
+    ArrayList<String> actorWriters = new ArrayList<String>(); //Used to store all writes for a movie
+    String plot;
+    String awards;
+    String posterLink;
+    //MovieRatings ciriticRating;
+}
