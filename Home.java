ab4cc6e70cc0e4c835c4f09b3bc51ec8e8f08c9e
GCoppel <102443015+GCoppel@users.noreply.github.com>
2022-03-30 20:53:41 -0500

    Add files via upload
    3/30 GUI Homepage (very rough)
    
    Has: Header bar with all elements, grid view of movies, a class for displaying said movies (still needs to connect to backend)

--- Home.java
+++ Home.java
@@ -15,6 +15,8 @@ public class Home {
         JButton buttonCollections = new JButton("Collections");
         JLabel labelAccount = new JLabel("Account");
 
+        //MEGAN -- Add a dropdown thingamabob here (some stuff at the bottom for you to work with, but it's very early stage)
+
         //Homepage Header Attributes (added from above)
         JPanel header = new JPanel();
         header.setLayout(new GridLayout(1,8));
@@ -24,26 +26,61 @@ public class Home {
         header.add(buttonFilter);
         header.add(buttonCollections);
         header.add(labelAccount);
-        header.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Home"));
 
-        //Testing MovieDisplay with blank Rectangle named "Movie"
-        MovieDisplay movieTest = new MovieDisplay("Movie");
+        //Testing MovieDisplay with blank Rectangles named "Movie_"
+        MovieDisplay movie1 = new MovieDisplay("Movie1");
+        MovieDisplay movie2 = new MovieDisplay("Movie2");
+        MovieDisplay movie3 = new MovieDisplay("Movie3");
+        MovieDisplay movie4 = new MovieDisplay("Movie4");
+        MovieDisplay movie5 = new MovieDisplay("Movie5");
+        MovieDisplay movie6 = new MovieDisplay("Movie6");
+        MovieDisplay movie7 = new MovieDisplay("Movie7");
+        MovieDisplay movie8 = new MovieDisplay("Movie8");
 
         frame.setLayout(new BorderLayout());
-        movieTest.setSize(30,40);
+
+        //Testing grid layout
+        movie1.setSize(90,140);
+        movie2.setSize(90,140);
+        movie3.setSize(90,140);
+        movie4.setSize(90,140);
+        movie5.setSize(90,140);
+        movie6.setSize(90,140);
+        movie7.setSize(90,140);
+        movie8.setSize(90,140);
+
+        String displayName = "Suggestions"; //Contains homepage grid name for display. Will be changed to show where movies are coming from (Collections, Search Results, etc.)
 
         JPanel movieGrid = new JPanel();
-        movieGrid.setLayout(new GridLayout(1,1));
+        movieGrid.setLayout(new GridLayout(2,4));
+        movieGrid.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), displayName));
 
+        //Testing grid layout
+        movieGrid.add(movie1);
+        movieGrid.add(movie2);
+        movieGrid.add(movie3);
+        movieGrid.add(movie4);
+        movieGrid.add(movie5);
+        movieGrid.add(movie6);
+        movieGrid.add(movie7);
+        movieGrid.add(movie8);
+
+
+        //Positioning homepage frame elements
         frame.add(movieGrid,BorderLayout.CENTER); //Adds MovieDisplay test to center of page
-
-        movieGrid.add(movieTest);
-
         frame.add(header, BorderLayout.PAGE_START);
         frame.setSize(500,500);
         frame.setLocationRelativeTo(null);
 
+        //Standard enabling and closing statements
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    //Contains the Filter Dropdown Menu, will be implemented into homepage soon when I've figured out how it works - George
+    public void filterDropDown() {
+        String[] options = {"Genre", "Age Rating", "Runtime"};
+
+        JComboBox<String> jComboBox = new JComboBox<>(options);
+        jComboBox.setBounds(80,50,140,20);
     }
 }
--- MovieDisplay.java
+++ MovieDisplay.java
@@ -6,23 +6,23 @@ import javax.swing.JPanel;
 
 public class MovieDisplay extends JPanel {
 
-    String movieName;
+    private String movieName;
 
     public MovieDisplay(String name) {
         this.movieName = name;
     }
 
-    //@Override
-    public void paintMovie(Graphics g) {
+    @Override
+    public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g; //cast it
-        g2d.drawRect(0, 0, 30, 40);
+        g2d.drawRect(0, 0, 90, 140);
         //g2d.clearRect(0,0,getWidth(),getHeight());
-        g2d.setColor(Color.red);
-        g2d.fillRect(0, 0, 30, 40);
+        g2d.setColor(Color.gray);
+        g2d.fillRect(0, 0, 90, 140);
 
         g2d.setColor(Color.black);
-        g2d.setFont(new Font("Comic Sans", Font.PLAIN, 20));
-        g2d.drawString(this.movieName, 0, 0);
+        g2d.setFont(new Font("Comic Sans", Font.PLAIN, 10));
+        g2d.drawString(this.movieName, 28, 160);
     }
 
 }
