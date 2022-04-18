628cef62da71541b3958187e976222dba15e48c4
GCoppel <georgelcoppe@gmail.com>
2022-04-18 16:12:14 -0500

    Brooklyn's stuff, redux

--- src/MainModel.java
+++
@@ -1,5 +0,0 @@
-package MainMVC;
-
-public class MainModel {
-    //TODO: Add logic for interacting with the "main" data (i.e. movie list).
-}
--- src/UserCollectionController.java
+++
@@ -1,5 +0,0 @@
-package UserCollectionMVC;
-
-public class UserCollectionController {
-    //TODO: Add logic for interaction between UserCollectionView and UserCollectionModel (i.e. what happens when add/remove movie buttons are clicked).
-}
