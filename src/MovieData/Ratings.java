5031809be693a7644846395762a9d8ec21415b91
GCoppel <georgelcoppe@gmail.com>
2022-04-18 16:12:30 -0500

    Brooklyn's stuff, redux

---
+++ src/MovieData/Ratings.java
@@ -0,0 +1,23 @@
+package MovieData;
+
+/**
+ * A class for creating and storing Ratings objects
+ */
+public class Ratings {
+    //VARIABLES
+    String Source;
+    String Value;
+
+    //GET FUNCTIONS
+    /**
+     * Returns the name of the Rating object
+     * @return the name of the rating source
+     */
+    public String getSource() {return Source;}
+
+    /**
+     * Returns the value of the Rating object
+     * @return the value of the rating
+     */
+    public String getValue() {return Value;}
+}
---
+++ src/UserCollectionMVC/UserCollectionView.java
@@ -0,0 +1,7 @@
+package UserCollectionMVC;
+
+import javax.swing.*;
+
+public class UserCollectionView extends JFrame {
+    //TODO: Add logic to display and add/remove movies and ratings to/from a user's collection. (i.e. display a list of movies in a collection with a button to add or remove a movie).
+}
